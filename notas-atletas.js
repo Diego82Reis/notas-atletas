59e79b876bfd74769bd029a8e22c98f122467df6
Diego Estevam <diego82.estevam@gmail.com>
2022-11-15 15:51:19 -0300

    meu primeiro commit

---
+++ notas-atletas.js
@@ -0,0 +1,86 @@
+// Definição da classe
+class NotasAtletas {
+    constructor(notasGeral){
+        this.notasGeral = notasGeral
+    }
+    obterNumeroAtletas(){
+        return this.notasGeral.length;
+    }
+    obterNomesAtleta(){
+        let nomes = [];
+        for (let i = 0; i < this.notasGeral.length; i++) {
+            nomes.push(this.notasGeral[i].nome);
+        }
+        return nomes;
+    }
+    obterNotasAtleta(){
+        let notasTodos = [];
+        for (let i = 0; i < this.notasGeral.length; i++) {
+            notasTodos.push("(" + this.notasGeral[i].notas + ")");
+        }
+        return notasTodos;
+    }
+    mediaGeral(){
+        let medias = [];
+        for (let i = 0; i < this.notasGeral.length; i++) {
+            let organizar = this.notasGeral[i].notas;
+            function comparaNumeros(a,b) {
+                if (a == b) return 0; 
+                if (a < b) return -1; 
+                if (a > b) return 1;
+                }
+            let ordem = organizar.sort(comparaNumeros);
+            let notasComputadas = ordem.slice(1,4)
+            let media = notasComputadas.reduce(function(total, atual){
+                return total + atual
+            },0);    
+           medias[i] = (media/notasComputadas.length).toFixed(2);
+        }
+        return medias;
+    }    
+    listaFinal(){
+       let nomes = [];
+       let notasTodos = [];
+       let medias = [];
+       let frase= [];
+       for (let i = 0; i < this.notasGeral.length; i++) {
+            nomes.push(this.notasGeral[i].nome);
+            notasTodos.push("(" + this.notasGeral[i].notas + ")");
+            let organizar = this.notasGeral[i].notas;
+            function comparaNumeros(a,b) {
+                if (a == b) return 0; 
+                if (a < b) return -1; 
+                if (a > b) return 1;
+                }
+            let ordem = organizar.sort(comparaNumeros);
+            let notasComputadas = ordem.slice(1,4)
+            let media = notasComputadas.reduce(function(total, atual){
+                return total + atual
+            },0)    
+           medias[i] = (media/notasComputadas.length).toFixed(2);
+        
+            frase[i] = console.log(`Atleta: ${nomes[i]}, Notas Obtidas: ${notasTodos[i]}, Médias Válidas: ${medias[i]}`);
+            }
+        }
+}
+// Uso da classe
+let atletas = [
+ {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas:  [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
+let notasAtletas = new NotasAtletas(atletas);
+notasAtletas.listaFinal();
---
+++ readme.md
@@ -0,0 +1,6 @@
+Esta ferramenta foi criada para ajudar o organizador de uma competição de ginástica artística na apuração da média das notas recebidas pelos atletas.
+Vários atletas realizam apresentações para uma banca composta por cinco jurados. Cada jurado é responsável por avaliar um critério em específico, sendo eles o tempo de duração da apresentação, originalidade da coreografia, postura do atleta, dificuldade das acrobacias e sincronismo.
+A competição em questão possui a seguinte regra de avaliação:
+Cada jurado pode fornecer uma nota de um (1) a dez (10);
+A média é calculada com base nas três notas do meio, desconsiderando a maior e menor nota.
+Para o código funcionar é só acrescentar na entrada de dados o nome e as notas recebidas dos jurados, executando o código, retornara as informações do atleta e a média de suas notas!
